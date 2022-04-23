c71fd2bc0179a607f2433975125dde252c0534c8
JONE <jone@JONEdeMacBook-Pro.local>
2022-04-23 23:51:10 +0800

    new concept about destructor and derived class

---
+++ c_c++/derivedClass.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+class Base 
+{
+    public:
+        int a, b, c;
+};
+
+class Derived : public Base
+{
+    public:
+        int b2;
+};
+
+class Derived2 : public Derived 
+{
+    int c2;
+};
+
+
+int main()
+{
+    Derived derived;
+    Derived2 deriv2;
+    derived.a = 1;
+    derived.b2 = 20;
+    deriv2.b = 30;
+    deriv2.b2 = 40;
+
+    cout << "Value from derived class: "<< derived.a << endl;
+ 
+    cout << "Value from derived class: "
+        << derived.b2 << endl;
+
+    cout << "Value from base class through derive2 class: "
+        << deriv2.b << endl;
+
+    cout << "Value from 1st derived class through derive2 class: "
+        << deriv2.b2 << endl;
+
+    return 0;
+}
---
+++ c_c++/destructor.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+class String
+{
+    private:
+        char * s;
+        int size;
+
+    public:
+        String();
+        String(char *); // constructor
+        ~String(); // destructor
+};
+
+String::String(char * c)
+{
+    size = strlen(c);
+    s = new char[size+1];
+    strcpy(s,c);
+}
+
+String::~String()
+{
+    delete [] s;
+}
+
+int main()
+{
+    String str;
+
+    
+
+    return 0;
+}
