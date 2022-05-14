a18a803c93d512093cf1a26dd6497849c293368a
tinolebat <jsdah07@gmail.com>
2022-05-14 13:43:58 +0800

    small changes

---
+++ c_c++/priorityQueue1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <limits.h>
+
+using namespace std;
+
+//  Using Linked list
+
+/* The entries are sorted in a descending order based on their priority */
+
+
+
+int main()
+{
+
+    return 0;
+}
